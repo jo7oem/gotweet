c19e5d6b79a79d847812e05f44fb001242664353
jo7oem <jo7oem@gmail.com>
2017-02-26 02:52:27 +0900

    fix

--- gotweet.go
+++ gotweet.go
@@ -7,22 +7,11 @@ import (
 )
 
 type TwitterApp struct {
-	consumer    oauth.Consumer
-	accessToken oauth.AccessToken
+	consumer    *oauth.Consumer
+	accessToken *oauth.AccessToken
 }
 
-func NewTwitterapp(APIkye, APIsec, accessToken, accessTokenSec string) *TwitterApp {
-	app := new(TwitterApp)
-	app.consumer = oauth.NewConsumer(
-		APIkye,
-		APIsec,
-		oauth.ServiceProvider{
-			RequestTokenUrl: "https://api.twitter.com/oauth/request_token",
-			AuthorizeTokenUrl: "	https://api.twitter.com/oauth/authorize",
-			AccessTokenUrl: "https://api.twitter.com/oauth/access_token"})
-	app.accessToken = &oauth.AccessToken{accessToken, accessTokenSec}
-	return app
-}
+
 func (t *TwitterApp) Get(url string, params map[string]string) (interface{}, error) {
 	response, err := t.consumer.Get(url, params, t.accessToken)
 	if err != nil {
